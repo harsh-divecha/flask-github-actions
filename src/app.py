df698ed676ce7be21050fc9dfc3fe6926747f0a0
Yashashri Naitam <yashashri_naitam@persistent.co.in>
2020-11-27 12:45:37 +0530

    Title changed

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<title>Test</title>Hello, Rohit!"
+    return "<title>Application</title>Hello, Rohit!"
 
 if __name__ == "__main__":
     app.run
